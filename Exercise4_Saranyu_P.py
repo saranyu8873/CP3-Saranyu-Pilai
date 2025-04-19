3ac88aeaa23b886988b6ff331d05ba80823c5209
saranyu8873 <saranyu_kup@windowslive.com>
2025-04-19 20:27:15 +0700

    Create Exercise4_Saranyu_P.py
    First Commit

---
+++ Exercise4_Saranyu_P.py
@@ -0,0 +1,10 @@
+fe = 60
+gb = 77
+ics = 65
+cp = 88
+
+print("---Your Score---")
+print("Foundation English :",fe,"/100")
+print("General Business :",gb,"/100")
+print("Introduction to Computer Systems :",ics,"/100")
+print("Computer Programming :",cp,"/100")
